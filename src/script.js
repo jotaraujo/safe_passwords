19cd714df8f17d0c753db88addc38f2188da41a2
João Paulo Araújo <jpaulofonseca99@gmail.com>
2025-08-29 15:32:38 -0300

    :sparkles: feat: Adicionando a funcionalidade de gerar senha

--- src/script.js
+++ src/script.js
@@ -1,20 +1,58 @@
 // Selecionar os elementos no DOM
+const generateButton = document.querySelector('#generate')
+const passwordSection = document.querySelector('#password')
+
+//Criar a função de letras maiúsculas
+const upperCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+//Criar a função de letras minúsculas
+const lowerCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+//Criar a função de números
+const numbers = () => {
+  return Math.floor(Math.random() * 10)
+}
+//Criar a função de caracteres especiais
+const symbols = () => {
+  const symbols = '!@#$%^&*()_-+=[]{}/,.<>'
+  return symbols[Math.floor(Math.random() * symbols.length)]
+}
 
 // Criar função para gerar a senha
-  //Verificar o tamanho da senha inserida pelo usuário
-  //Verificar se deve incluir letras maiúsculas na senha
-    //Se sim, gerar novas letras maiúsculas aleatoriamente
-  //Verificar se deve incluir letras minúsculas na senha
-    //Se sim, gerar novas letras minúsculas aleatoriamente
-  //Verificar se deve incluir números na senha
-    //Se sim, gerar números aleatoriamente
-  //Verificar se deve incluir caracteres especiais na senha
-    //Se sim, gerar novos caracteres especiais aleatoriamente
-  //Criar uma variável para guardar a nova senha gerada
+// const generatePassword = () => {
+//   const password = []
+
+// }
+//Verificar o tamanho da senha inserida pelo usuário
+//Verificar se deve incluir letras maiúsculas na senha
+//Se sim, chamar a função de letras maiúsculas
+//Verificar se deve incluir letras minúsculas na senha
+//Se sim, chamar a função de letras minúsculas
+//Verificar se deve incluir números na senha
+//Se sim, chmar a função de números
+//Verificar se deve incluir caracteres especiais na senha
+//Se sim, chamar a função de caracteres especiais
+//Criar uma variável para guardar a nova senha gerada
 
 
 //Criar função para gerar os elementos da div password
-  //Selecionar a div password
-  //Criar um h1 para a senha gerada chamando a função gerarSenha
-  //Criar um p para falar que a senha foi gerada com sucesso
-  //Criar um botão para copiar a senha
+const createPassword = (uppercase, lowercase, number, symbol) => {
+  const password = [uppercase, lowercase, number, symbol]
+  const title = document.createElement('h2')
+  title.innerText = password
+
+  passwordSection.append(title)
+}
+//Selecionar a div password
+//Criar um h1 para a senha gerada chamando a função gerarSenha
+//Criar um p para falar que a senha foi gerada com sucesso
+//Criar um botão para copiar a senha
+
+// Adicionar o evento de clique ao botão de gerar senha
+generateButton.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  createPassword(upperCase(), lowerCase(), numbers(), symbols())
+})
