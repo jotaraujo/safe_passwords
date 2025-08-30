0c692947b2d51f8c641bbc2d078cdee0f5e02e8b
João Paulo Araújo <jpaulofonseca99@gmail.com>
2025-08-30 00:15:37 -0300

    :sparkles: feat: Adicionando funcionalidade de copiar senha

--- src/script.js
+++ src/script.js
@@ -1,26 +1,27 @@
-// Selecionar os elementos no DOM
 const generateButton = document.querySelector('#generate')
 const passwordSection = document.querySelector('#password-section')
+const uppercaseInput = document.querySelector('#uppercase')
+const lowercaseInput = document.querySelector('#lowercase')
+const numbersInput = document.querySelector('#numbers')
+const symbolsInput = document.querySelector('#symbols')
 
-//Criar a função de letras maiúsculas
 const upperCase = () => {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
 }
-//Criar a função de letras minúsculas
+
 const lowerCase = () => {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
-//Criar a função de números
+
 const numbers = () => {
   return Math.floor(Math.random() * 10)
 }
-//Criar a função de caracteres especiais
+
 const symbols = () => {
   const symbols = '!@#$%^&*()_-+=[]{}/.<>'
   return symbols[Math.floor(Math.random() * symbols.length)]
 }
 
-// Criar função para gerar a senha
 const generatePassword = (upperCase, lowerCase, numbers, symbols) => {
   let password = ""
 
@@ -42,19 +43,7 @@ const generatePassword = (upperCase, lowerCase, numbers, symbols) => {
   password = password.slice(0, passwordLength)
   createPassword(password)
 }
-//Verificar o tamanho da senha inserida pelo usuário
-//Verificar se deve incluir letras maiúsculas na senha
-//Se sim, chamar a função de letras maiúsculas
-//Verificar se deve incluir letras minúsculas na senha
-//Se sim, chamar a função de letras minúsculas
-//Verificar se deve incluir números na senha
-//Se sim, chmar a função de números
-//Verificar se deve incluir caracteres especiais na senha
-//Se sim, chamar a função de caracteres especiais
-//Criar uma variável para guardar a nova senha gerada
 
-
-//Criar função para gerar os elementos da div password
 const createPassword = (data) => {
   const password = document.createElement('h2')
   password.classList.add('password')
@@ -67,16 +56,15 @@ const createPassword = (data) => {
   const copyButton = document.createElement('button')
   copyButton.classList.add('copy')
   copyButton.innerText = 'Copy password'
+  copyButton.addEventListener('click', () => {
+    navigator.clipboard.writeText(password.innerText)
+    copyButton.innerText = 'Password copied!'
+  })
 
   passwordSection.style.display = 'block'
   passwordSection.append(successMsg, password, copyButton)
 }
-//Selecionar a div password
-//Criar um h1 para a senha gerada chamando a função gerarSenha
-//Criar um p para falar que a senha foi gerada com sucesso
-//Criar um botão para copiar a senha
 
-// Adicionar o evento de clique ao botão de gerar senha
 generateButton.addEventListener('click', (e) => {
   e.preventDefault()
 
